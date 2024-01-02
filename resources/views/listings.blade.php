8d386fbef6a05ffd634925813ba918eb9d507473
AntipovIvan <85480386+AntipovIvan@users.noreply.github.com>
2024-01-02 22:46:15 +0900

    1:35 handle tags

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,22 +1,32 @@
-<h1>{{ $heading }}</h1>
+@extends('layout')
 
-{{-- Dirty filtering --}}
-{{-- @php
+@section('content')
+
+    @include('partials._hero')
+    @include('partials._search')
+
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+        {{-- Dirty filtering --}}
+        {{-- @php
     $test = 1;
 @endphp
 {{ $test }} --}}
 
-{{-- @if (count($listings) == 0)
+        {{-- @if (count($listings) == 0)
     <p>No listings found</p>
 @endif --}}
 
-@unless (count($listings) == 0)
-    @foreach ($listings as $listing)
-        <h2>
-            <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a>
-        </h2>
-        <p>{{ $listing['description'] }}</p>
-    @endforeach
-@else
-    <p>No listings found</p>
-@endunless
+        @unless (count($listings) == 0)
+            @foreach ($listings as $listing)
+                {{-- <h2>
+                    <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a>
+                </h2>
+                <p>{{ $listing['description'] }}</p> --}}
+                <x-listing-card :listing="$listing" />
+            @endforeach
+        @else
+            <p>No listings found</p>
+        @endunless
+    </div>
+@endsection
